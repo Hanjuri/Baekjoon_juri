0121bc3fbacf6cf8746d9f54c18a394fe6f5ae70
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2024-11-14 18:11:36 +0900

    Title: 사각형 채우기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/사각형 채우기/rectangle-fill.py
+++ 241114/사각형 채우기/rectangle-fill.py
@@ -5,6 +5,6 @@ def solution(n):
     if n > 2:
         for i in range(3,n+1):
             dp[i] = dp[i-1] + dp[i-2]
-    return dp[n]
+    return dp[n] % 10007
 n = int(input())
 print(solution(n))
