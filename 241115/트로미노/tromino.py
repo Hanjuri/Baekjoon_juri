f602851e91baa675f5a744e570dcb4c0a82bb7f7
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2024-11-15 18:54:15 +0900

    Title: 트로미노, Time: 74ms, Memory: 18MB, Status: Passed - CodeTree

--- 241115/트로미노/tromino.py
+++ 241115/트로미노/tromino.py
@@ -1,33 +1,26 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-
 total_answer = []
 for i in range(n):
     for j in range(m):
-        best = [] #0 0
+        best = set() #0 0
         if i + 2 < n:
             sum = 0
             for e in range(3):
                 sum += arr[i+e][j]  
-            best.append(sum)
+            best.add(sum)
 
         if j + 2 < m:
             sum = 0
             for e in range(3):
                 sum += arr[i][j+e]
-            best.append(sum)
-        if 0 <= j-1 & 0 <= i-1:
-            sum = 0
-            sum += arr[i][j]
-            sum += arr[i-1][j]
-            sum += arr[i][j-1]
-            best.append(sum)
-        if j+1 < m & i+1 < n:
-            sum = 0
-            sum += arr[i][j]
-            sum += arr[i+1][j]
-            sum += arr[i][j+1]
-            best.append(sum)
-        total_answer.append(max(best))
+            best.add(sum)
+        if i + 1 < n and j + 1 < m:
+            best.add(arr[i][j] + arr[i+1][j] + arr[i][j+1])
+            best.add(arr[i+1][j] + arr[i][j+1] + arr[i+1][j+1])
+            best.add(arr[i][j] + arr[i+1][j] + arr[i+1][j+1] )
+            best.add(arr[i][j] +arr[i][j+1] + arr[i+1][j+1] )
+        if best:
+            total_answer.append(max(best))
 
 print(max(total_answer))
