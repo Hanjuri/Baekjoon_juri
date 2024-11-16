eac2c267c166fde6a83e1013a77cda2f3d524d4e
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2024-11-16 20:53:42 +0900

    Title: 삼각형 컨베이어 벨트, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241116/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 241116/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -4,7 +4,7 @@ arr2 = [[0]* len(arr[0]) for _ in range(len(arr))]
 
 for i in range(len(arr)):
   for j in range(n):
-    new_i = (i+ (j+t)//n) % 3
+    new_i = (i+ (j+t)//n) % len(arr)
     new_j = (j+t) % n
     arr2[new_i][new_j] = arr[i][j]
 
