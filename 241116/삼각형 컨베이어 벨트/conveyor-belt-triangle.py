1374124ec6ecee9726117992fa550e81e6f2edb5
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2024-11-16 20:53:13 +0900

    Title: 삼각형 컨베이어 벨트, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241116/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 241116/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -1,6 +1,7 @@
 n, t = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 arr2 = [[0]* len(arr[0]) for _ in range(len(arr))]
+
 for i in range(len(arr)):
   for j in range(n):
     new_i = (i+ (j+t)//n) % 3
