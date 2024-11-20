854a0dc127de06bce90f68ee6e7a4f56f82a2d9e
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2024-11-20 10:20:28 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 692ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 241120/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+// 변수 선언 및 입력
+const [k, n] = input.split(" ").map(Number);
+
+function printAnswer(arr){
+    console.log(arr.join(' '));
+}
+function allElementsTree(arr){
+    let count = 1;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === arr[i-1]){
+            count++;
+            if (count >= 3){
+                return true;
+            }
+        }else{
+            count = 1;
+        }
+    }
+    return false;
+}
+
+function sol(cnt, ans){
+    if(cnt === n){
+        if(n === 1){
+            printAnswer(ans);
+        }else{
+            if(!allElementsTree(ans)){
+                printAnswer(ans);
+            }
+        }
+        return;
+    }
+    for (let i = 1; i < k+1; i++) {
+        ans.push(i);
+        sol(cnt+1, ans);
+        ans.pop();
+    }
+}
+sol(0,[]);
