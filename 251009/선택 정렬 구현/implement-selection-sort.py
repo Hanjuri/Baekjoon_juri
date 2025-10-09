5fb2a830f71243859f27707ba3352744ecac5721
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2025-10-09 14:24:13 +0900

    Title: 선택 정렬 구현, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251009/선택 정렬 구현/implement-selection-sort.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        min = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min]:
+                min = j
+        arr[i], arr[min] = arr[min], arr[i]
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+selection_sort(arr)
+print(*arr)
